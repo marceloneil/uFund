bc9b33c58f65497fb541724a49073ce1ed495ee2
Marcel O'Neil <marcel@marceloneil.com>
2016-10-15 21:10:42 -0400

    Contract

--- app.js
+++ app.js
@@ -26,12 +26,14 @@ var shareholder = web3.eth.contract(shareholderABI);
 
 var master = "0x32724503BDC7279409f4D723Bec8B5cA9f95d964";
 var successAddress = "0x2b645AE3bce483B26B65B934513aF0228F46BDfa";
+var shareContract;
 var shareAddress;
 var shareholderAddress;
 var saleAddress;
 var tokenNumber = 100;
+var tokenSelling = 40;
 var tokenName = "Share";
-var fundingGoal = 100;
+var fundingGoal = 40;
 var duration = 60;
 var cost = 1;
 
@@ -42,32 +44,44 @@ web3.personal.unlockAccount(master, "testtesttest", 1000);
 share.new(tokenNumber, tokenName, 0, "µ", {
     data: shareBC,
     from: master,
-    gas: web3.eth.estimateGas({data: shareBC}) + 100000
+    gas: web3.eth.estimateGas({
+        data: shareBC
+    }) + 100000
 }, function(err, contract) {
     if (!err) {
         if (contract.address) {
             shareAddress = contract.address;
+            shareContract = contract;
             shareholder.new(Math.floor(tokenNumber / 4), 1500, shareAddress, {
                 data: shareholderBC,
                 from: master,
-                gas: web3.eth.estimateGas({data: shareholderBC}) + 100000
+                gas: web3.eth.estimateGas({
+                    data: shareholderBC
+                }) + 100000
             }, function(err, contract) {
                 if (!err) {
                     if (contract.address) {
                         shareholderAddress = contract.address;
-                        //console.log(successAddress, fundingGoal, duration, cost, shareAddress);
                         sale.new(successAddress, fundingGoal, duration, cost, shareAddress, {
                             data: saleBC,
                             from: master,
-                            gas: web3.eth.estimateGas({data: saleBC}) + 100000
+                            gas: web3.eth.estimateGas({
+                                data: saleBC
+                            }) + 100000
                         }, function(err, contract) {
                             if (!err) {
                                 if (contract.address) {
                                     saleAddress = contract.address;
 
-                                    console.log(shareAddress);
-                                    console.log(shareholderAddress);
-                                    console.log(saleAddress);
+                                    console.log("Add " + shareAddress + " to Custom Tokens");
+                                    console.log("Send money to " + saleAddress);
+
+                                    shareContract.transfer(saleAddress, tokenSelling, {
+                                        from: master
+                                    });
+                                    shareContract.transfer(successAddress, tokenNumber - tokenSelling, {
+                                        from: master
+                                    });
                                 }
                             } else {
                                 console.log(err);
